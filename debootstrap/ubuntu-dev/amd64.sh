362ee74a1f1ebec21b8437aaa033ca6ab34bad07
2moe <25324935+2moe@users.noreply.github.com>
2021-01-21 04:28:04 +0800

    DISTRO_CODE

--- debootstrap/ubuntu-dev/amd64.sh
+++ debootstrap/ubuntu-dev/amd64.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 #####################
 CUR=${PWD}
-unset DISRO_CODE
+unset DISTRO_CODE
 DEBIAN_CHROOT="ubuntu"
 ARCH_TYPE=amd64
 UBUNTU_URL="http://azure.archive.ubuntu.com/ubuntu"
 UBUNTU_URL_02="https://mirrors.bfsu.edu.cn/ubuntu"
-DISRO_CODE=$(curl -L ${UBUNTU_URL}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
-[[ -n ${DISRO_CODE} ]] || DISRO_CODE=$(curl -L ${UBUNTU_URL_02}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
-echo ${DISRO_CODE}
+DISTRO_CODE=$(curl -L ${UBUNTU_URL}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
+[[ -n ${DISTRO_CODE} ]] || DISTRO_CODE=$(curl -L ${UBUNTU_URL_02}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
+echo ${DISTRO_CODE}
 ###################
 sudo apt update
 sudo apt install -y debootstrap
@@ -24,7 +24,7 @@ sudo debootstrap --no-check-gpg --arch ${ARCH_TYPE} --components=main,restricted
 sudo mkdir -pv ${DEBIAN_CHROOT}/run/systemd
 sudo su -c "echo 'docker' >${DEBIAN_CHROOT}/run/systemd/container"
 
-sed -i "s@hirsute@${DISRO_CODE}@g" ${ARCH_TYPE}.list
+sed -i "s@hirsute@${DISTRO_CODE}@g" ${ARCH_TYPE}.list
 sudo cp -fv ${ARCH_TYPE}.list ${DEBIAN_CHROOT}/etc/apt/sources.list
 
 cd ${DEBIAN_CHROOT}
--- debootstrap/ubuntu-dev/arm64.sh
+++ debootstrap/ubuntu-dev/arm64.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 #####################
 CUR=${PWD}
-unset DISRO_CODE
+unset DISTRO_CODE
 DEBIAN_CHROOT="ubuntu"
 ARCH_TYPE=arm64
 UBUNTU_URL="http://ports.ubuntu.com/ubuntu-ports/"
 UBUNTU_URL_02="https://mirrors.bfsu.edu.cn/ubuntu-ports/"
-DISRO_CODE=$(curl -L ${UBUNTU_URL}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
-[[ -n ${DISRO_CODE} ]] || DISRO_CODE=$(curl -L ${UBUNTU_URL_02}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
+DISTRO_CODE=$(curl -L ${UBUNTU_URL}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
+[[ -n ${DISTRO_CODE} ]] || DISTRO_CODE=$(curl -L ${UBUNTU_URL_02}/dists/devel/Release | grep 'Codename:' | head -n 1 | awk -F ': ' '{print $2}')
 ###################
 sudo apt update
 sudo apt install -y debootstrap qemu-user-static
@@ -22,7 +22,7 @@ sudo debootstrap --no-check-gpg --arch ${ARCH_TYPE} --components=main,restricted
 sudo mkdir -pv ${DEBIAN_CHROOT}/run/systemd
 sudo su -c "echo 'docker' >${DEBIAN_CHROOT}/run/systemd/container"
 
-sed -i "s@hirsute@${DISRO_CODE}@g" ${ARCH_TYPE}.list
+sed -i "s@hirsute@${DISTRO_CODE}@g" ${ARCH_TYPE}.list
 sudo cp -fv ${ARCH_TYPE}.list ${DEBIAN_CHROOT}/etc/apt/sources.list
 
 cd ${DEBIAN_CHROOT}
